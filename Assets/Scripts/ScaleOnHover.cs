044e421ec6c65dc2185244f830e35d7a6f99ece3
Rees Morris <reesmorriscontact@gmail.com>
2018-10-25 12:45:39 +0100

    Added ScaleOnHover functionality

---
+++ Assets/Scripts/ScaleOnHover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ScaleOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    public Vector3 growthSize;
+
+    private RectTransform rectTransform;
+    private Vector3 initialSize;
+
+	// Use this for initialization
+	void Start () {
+        rectTransform = GetComponent<RectTransform>();
+        initialSize = rectTransform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        rectTransform.localScale = initialSize + growthSize;
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        rectTransform.localScale = initialSize;
+    }
+}
