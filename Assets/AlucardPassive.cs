cbb7ac97935a51817aa2221bb185b0a50b2f9867
Rees Morris <reesmorriscontact@gmail.com>
2019-04-06 01:14:33 +0100

    Added Alucard passive ability

---
+++ Assets/AlucardPassive.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlucardPassive : MonoBehaviour {
+
+    PhotonView photonView;
+    PlayerChampion champion;
+
+    void Start() {
+        Entity.onEntityDeath += OnEntityDeath;
+        photonView = GetComponent<PhotonView>();
+        champion = GetComponent<PlayerChampion>();
+    }
+
+    void OnEntityDeath(int attackerId) {
+        if(photonView.isMine) {
+            if(photonView.viewID == attackerId) {
+                photonView.RPC("Heal", PhotonTargets.All, champion.Champion.maxHealth * 0.07f);
+                Debug.Log("Healed by " + champion.Champion.maxHealth * 0.03f);
+            }
+        }
+    }
+}
